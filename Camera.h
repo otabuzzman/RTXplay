d5ca28367ad1c6878396923fd488a51930d252c1
otabuzzman <iuergen.schuck@gmail.com>
2020-12-04 08:33:35 +0100

    Update RTOW chapter 12

--- Camera.h
+++ Camera.h
@@ -4,27 +4,30 @@
 #include "util.h"
 
 class Camera {
-    public:
-        Camera( P pos, P pov, V up, double angle, double ratio ) {
-			auto viewh = 2.*tan( deg2rad( angle )/2 ) ; // virtual viewport height
-			auto vieww = ratio*viewh ;                  // virtual viewport width
-			auto w = unitV( pos-pov ) ;
-			auto u = unitV( cross( up, w ) ) ;
-			auto v = cross( w, u ) ;
+	public:
+		Camera( P pos, P pov, V up, double fov, double ratio, double aperture, double distance ) {
+			auto viewh = 2.*tan( deg2rad( fov )/2 ) ; // virtual viewport height
+			auto vieww = ratio*viewh ;                // virtual viewport width
+			w = unitV( pos-pov ) ;
+			u = unitV( cross( up, w ) ) ;
+			v = cross( w, u ) ;
 
 			orig = pos ;                              // camera origin
-			hori = vieww*u ;
-			vert = viewh*v ;
-			bole = orig-hori/2-vert/2-w ;             // bottom left viewport corner
+			hori = distance*vieww*u ;
+			vert = distance*viewh*v ;
+			bole = orig-hori/2-vert/2-distance*w ;    // bottom left viewport corner
+			lnsr = aperture/2 ;                       // lens radius
 		}
 
-		Ray ray( double s, double t ) const { return Ray( orig, bole+s*hori+t*vert-orig ) ; }
+		Ray ray( double s, double t ) const { V r = lnsr*rndVin1disk() ; V o = r.x()*u+r.y()*v ; return Ray( orig+o, bole+s*hori+t*vert-orig-o ) ; }
 
 	private:
 		P orig ;
 		P bole ;
 		V hori ;
 		V vert ;
+		V u, v, w ;
+		double lnsr ;
 } ;
 
 #endif
