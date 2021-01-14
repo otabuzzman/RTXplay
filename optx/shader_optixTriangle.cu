a6095b953b5ca81eb914963f1eb37308ed2004b8
otabuzzman <iuergen.schuck@gmail.com>
2021-01-14 10:38:57 +0100

    Simplify result color handling

--- optx/shader_optixTriangle.cu
+++ optx/shader_optixTriangle.cu
@@ -37,28 +37,13 @@ using V::operator+ ;
 using V::operator- ;
 using V::operator* ;
 
-__forceinline__ __device__ float3 toSRGB( const float3& c )
+__forceinline__ __device__ uchar4 sRGB( const float3& c )
 {
-    float  invGamma = 1.0f / 2.4f;
-    float3 powed    = make_float3( powf( c.x, invGamma ), powf( c.y, invGamma ), powf( c.z, invGamma ) );
-    return make_float3(
-        c.x < 0.0031308f ? 12.92f * c.x : 1.055f * powed.x - 0.055f,
-        c.y < 0.0031308f ? 12.92f * c.y : 1.055f * powed.y - 0.055f,
-        c.z < 0.0031308f ? 12.92f * c.z : 1.055f * powed.z - 0.055f );
-}
-
-__forceinline__ __device__ unsigned char quantizeUnsigned8Bits( float x )
-{
-    x = util::clamp( x, 0.0f, 1.0f );
-    enum { N = (1 << 8) - 1, Np1 = (1 << 8) };
-    return (unsigned char)min((unsigned int)(x * (float)Np1), (unsigned int)N);
-}
-
-__forceinline__ __device__ uchar4 make_color( const float3& c )
-{
-    // first apply gamma, then convert to unsigned char
-    float3 srgb = toSRGB( util::clamp( c, 0.0f, 1.0f ) );
-    return make_uchar4( quantizeUnsigned8Bits( srgb.x ), quantizeUnsigned8Bits( srgb.y ), quantizeUnsigned8Bits( srgb.z ), 255u );
+    return make_uchar4(
+        // README.md#Findings#3
+        (unsigned char) ( util::clamp( c.x, .0f, 1.f )*255.f+.5f ),
+        (unsigned char) ( util::clamp( c.y, .0f, 1.f )*255.f+.5f ),
+        (unsigned char) ( util::clamp( c.z, .0f, 1.f )*255.f+.5f ), 255u ) ;
 }
 
 // README.md#Findings#2
@@ -117,7 +102,7 @@ extern "C" __global__ void __raygen__rg()
     result.z = int_as_float( p2 );
 
     // Record results in our output raster
-    params.image[idx.y * params.image_width + idx.x] = make_color( result );
+    params.image[idx.y * params.image_width + idx.x] = sRGB( result );
 }
 
 extern "C" __global__ void __miss__ms()
