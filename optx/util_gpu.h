9e42e9c002863dcaa1322baff606cba3b3ae15b5
otabuzzman <iuergen.schuck@gmail.com>
2021-02-03 20:00:09 +0100

    Add random-functionality to GPU code

--- optx/util_gpu.h
+++ optx/util_gpu.h
@@ -1,7 +1,13 @@
 #ifndef UTIL_GPU_H
 #define UTIL_GPU_H
 
+#include <curand.h>
+#include <curand_kernel.h>
+
 namespace util {
+
+__forceinline__ __device__ float rnd( curandState* state )                                   { return static_cast<float>( curand( state ) )/( static_cast<float>( std::numeric_limits<unsigned int>::max() )+1.f ) ; }
+__forceinline__ __device__ float rnd( const float min, const float max, curandState* state ) { return min+rnd( state )*( max-min ) ; }
 
 __forceinline__ __device__ float  clamp( const float   x, const float min, const float max ) { return min>x ? min : x>max ? max : x ; }
 __forceinline__ __device__ float3 clamp( const float3& x, const float min, const float max ) { return make_float3( clamp( x.x, min, max ), clamp( x.y, min, max ), clamp( x.z, min, max ) ) ; }
