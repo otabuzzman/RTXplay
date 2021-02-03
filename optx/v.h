531b1ebdd1f2522d3d0bf7b1bd56b50bd05982ea
otabuzzman <iuergen.schuck@gmail.com>
2021-02-03 20:18:58 +0100

    Add random-functionality to GPU code

--- optx/v.h
+++ optx/v.h
@@ -16,21 +16,21 @@ namespace V {
 __forceinline__ __host__ __device__ float3 operator +  ( const float a,   const float3& b ) { return make_float3( a+b.x, a+b.y, a+b.z ) ; }
 __forceinline__ __host__ __device__ float3 operator +  ( const float3& a, const float b   ) { return make_float3( a.x+b, a.y+b, a.z+b ) ; }
 __forceinline__ __host__ __device__ float3 operator +  ( const float3& a, const float3& b ) { return make_float3( a.x+b.x, a.y+b.y, a.z+b.z ) ; }
-__forceinline__ __host__ __device__ void   operator += ( float3& a, const float3& b ) { a.x+=b.x ; a.y+=b.y ; a.z+=b.z ; }
+__forceinline__ __host__ __device__ void   operator += ( float3& a, const float3& b )       { a.x+=b.x ; a.y+=b.y ; a.z+=b.z ; }
 
 __forceinline__ __host__ __device__ float3 operator -  ( const float a,   const float3& b ) { return make_float3( a-b.x, a-b.y, a-b.z ) ; }
 __forceinline__ __host__ __device__ float3 operator -  ( const float3& a, const float b   ) { return make_float3( a.x-b, a.y-b, a.z-b ) ; }
 __forceinline__ __host__ __device__ float3 operator -  ( const float3& a, const float3& b ) { return make_float3( a.x-b.x, a.y-b.y, a.z-b.z ) ; }
-__forceinline__ __host__ __device__ void   operator -= ( float3& a, const float3& b ) { a.x-=b.x ; a.y-=b.y ; a.z-=b.z ; }
+__forceinline__ __host__ __device__ void   operator -= ( float3& a, const float3& b )       { a.x-=b.x ; a.y-=b.y ; a.z-=b.z ; }
 
 __forceinline__ __host__ __device__ float3 operator *  ( const float a,   const float3& b ) { return make_float3( a*b.x, a*b.y, a*b.z ) ; }
 __forceinline__ __host__ __device__ float3 operator *  ( const float3& a, const float b   ) { return make_float3( a.x*b, a.y*b, a.z*b ) ; }
 __forceinline__ __host__ __device__ float3 operator *  ( const float3& a, const float3& b ) { return make_float3( a.x*b.x, a.y*b.y, a.z*b.z ) ; }
-__forceinline__ __host__ __device__ void   operator *= ( float3& a, const float3& b ) { a.x*=b.x ; a.y*=b.y ; a.z*=b.z ; }
+__forceinline__ __host__ __device__ void   operator *= ( float3& a, const float3& b )       { a.x*=b.x ; a.y*=b.y ; a.z*=b.z ; }
 
 __forceinline__ __host__ __device__ float3 operator /  ( const float a,   const float3& b ) { return make_float3( a/b.x, a/b.y, a/b.z ) ; }
 __forceinline__ __host__ __device__ float3 operator /  ( const float3& a, const float b   ) { return 1.f/b*a ; }
-__forceinline__ __host__ __device__ void   operator /= ( float3& a, const float b   ) { 1.f/b*a ; }
+__forceinline__ __host__ __device__ void   operator /= ( float3& a, const float b   )       { 1.f/b*a ; }
 __forceinline__ __host__ __device__ float3 operator /  ( const float3& a, const float3& b ) { return make_float3( a.x/b.x, a.y/b.y, a.z/b.z ) ; }
 
 __forceinline__ __host__ __device__ float  dot  ( const float3& a, const float3& b ) { return a.x*b.x+a.y*b.y+a.z*b.z ; }
@@ -38,10 +38,7 @@ __forceinline__ __host__ __device__ float  len  ( const float3& v )             
 __forceinline__ __host__ __device__ float3 cross( const float3& a, const float3& b ) { return make_float3( a.y*b.z-a.z*b.y, a.z*b.x-a.x*b.z, a.x*b.y-a.y*b.x ) ; }
 __forceinline__ __host__ __device__ float3 unitV( const float3& v )                  { return 1.f/len( v )*v ; }
 
-__forceinline__ __host__ __device__ float3 near0( const float3& v )                  { return ( fabsf(v.x )<1e-8 ) && ( fabsf( v.y )<1e-8 ) && ( fabsf( v.z )<1e-8 ) ; }
-
-inline float3 rnd()                                   { return make_float3( util::rnd(), util::rnd(), util::rnd() ) ; }
-inline float3 rnd( const float min, const float max ) { return make_float3( util::rnd( min, max ), util::rnd( min, max ), util::rnd( min, max ) ) ; }
+__forceinline__ __host__ __device__ bool   near0( const float3& v )                  { return ( fabsf(v.x )<1e-8 ) && ( fabsf( v.y )<1e-8 ) && ( fabsf( v.z )<1e-8 ) ; }
 
 __forceinline__ __device__ float3 rnd( curandState *state )                                   { return make_float3( util::rnd( state ), util::rnd( state ), util::rnd( state ) ) ; }
 __forceinline__ __device__ float3 rnd( const float min, const float max, curandState *state ) { return make_float3( util::rnd( min, max, state ), util::rnd( min, max, state ), util::rnd( min, max, state ) ) ; }
