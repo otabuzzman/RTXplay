9b7444a8c48ab7bfddd58d7a8d5508b6e5f8beed
otabuzzman <iuergen.schuck@gmail.com>
2021-02-08 16:38:37 +0100

    Save work on diffuse shader

--- optx/shader_all.cu
+++ optx/shader_all.cu
@@ -39,27 +39,25 @@ using V::operator* ;
 
 extern "C" { __constant__ LpGeneral lp_general ; }
 
-static __forceinline__ __device__ uchar4 sRGB( const float3& color ) {
+static __forceinline__ __device__ uchar4 sRGB( const float r, const float g, const float b ) {
 	return make_uchar4(
-		(unsigned char) ( util::clamp( color.x, .0f, 1.f )*255.f+.5f ),
-		(unsigned char) ( util::clamp( color.y, .0f, 1.f )*255.f+.5f ),
-		(unsigned char) ( util::clamp( color.z, .0f, 1.f )*255.f+.5f ), 255u ) ;
+		(unsigned char) ( util::clamp( r, .0f, 1.f )*255.f+.5f ),
+		(unsigned char) ( util::clamp( g, .0f, 1.f )*255.f+.5f ),
+		(unsigned char) ( util::clamp( b, .0f, 1.f )*255.f+.5f ), 255u ) ;
 }
 
 extern "C" __global__ void __raygen__camera() {
-	const uint3 idx = optixGetLaunchIndex() ;
+	// dim.x/ dim.y correspond to image width/ height
 	const uint3 dim = optixGetLaunchDimensions() ;
+	// idx.x/ idx.y correspond to this pixel
+	const uint3 idx = optixGetLaunchIndex() ;
 
-	// set pixel index
+	// calculate pixel index for image buffer access
 	const unsigned int pix = dim.x*idx.y+idx.x ;
 
 	// initialize random number generator
 	curandState state ;
 	curand_init( 4711, pix, 0, &state ) ;
-
-	// prepare RNG state pointer for payload transmission
-	unsigned int sh = reinterpret_cast<unsigned long long>( &state )>>32 ;
-	unsigned int sl = reinterpret_cast<unsigned long long>( &state )&0x00000000ffffffff ;
 
 	// transform x/y pixel coords (range 0/0 to w/h)
 	// into s/t viewport coords (range -1/-1 to 1/1)
@@ -72,35 +70,40 @@ extern "C" __global__ void __raygen__camera() {
 	float3 ori, dir ;
 	camera->ray( s, t, ori, dir, &state ) ;
 
-	// initialize depth count
+	// payloads to carry back color from abyss of trace
+	unsigned int r, g, b ;
+
+	// payloads to propagate RNG state down the trace
+	// pointer split due to 32 bit limit of payload values
+	unsigned int sh = reinterpret_cast<unsigned long long>( &state )>>32 ;
+	unsigned int sl = reinterpret_cast<unsigned long long>( &state )&0x00000000ffffffff ;
+
+	// payload to propagate depth count down the trace
 	unsigned int depth = 1 ;
 
-	// trace into scene
-	unsigned int r, g, b ;
+	// shoot initial ray
 	optixTrace(
 			lp_general.as_handle,
-			ori,
-			dir,
-			1e-3f,                      // Min intersection distance
-			1e16f,                      // Max intersection distance
-			0.f,                        // rayTime -- used for motion blur
-			OptixVisibilityMask( 255 ), // Specify always visible
+			ori,                         // ray position
+			dir,                         // ray direction
+			1e-3f,                       // tmin
+			1e16f,                       // tmax
+			0.f,                         // motion
+			OptixVisibilityMask( 255 ),
 			OPTIX_RAY_FLAG_NONE,
-			0,                          // SBT offset   -- See SBT discussion
-			1,                          // SBT stride   -- See SBT discussion
-			0,                          // missSBTIndex -- See SBT discussion
-			r, g, b,
-			sh, sl,
-			depth
+			0,                           // SBT related
+			1,                           // SBT related
+			0,                           // SBT related
+			r, g, b, // payload upstream: color
+			sh, sl,  // payload downstream: RNG state pointer
+			depth    // payload downstream: recursion depth
 			) ;
 
-	const float3 color = make_float3(
-			__uint_as_float( r ),
-			__uint_as_float( g ),
-			__uint_as_float( b )
-			) ;
-
-	lp_general.image[pix] = sRGB( color ) ;
+	// update pixel in image buffer with this ray's color
+	lp_general.image[pix] = sRGB(
+		__uint_as_float( r ),
+		__uint_as_float( g ),
+		__uint_as_float( b ) ) ;
 }
 
 extern "C" __global__ void __miss__ambient() {
@@ -110,6 +113,7 @@ extern "C" __global__ void __miss__ambient() {
 	// get this ray's direction from OptiX and normalize
 	const float3 unit    = V::unitV( optixGetWorldRayDirection() ) ;
 
+	// calculate background color according to RTOW
 	const float t        = .5f*( unit.y+1.f ) ;
 	const float3 white   = { 1.f, 1.f, 1.f } ;
 	const float3 color   = ( 1.f-t )*white+t*ambient ;
@@ -120,25 +124,32 @@ extern "C" __global__ void __miss__ambient() {
 }
 
 extern "C" __global__ void __closesthit__diffuse() {
+	// retrieve actual trace depth from payload
 	unsigned int depth = optixGetPayload_5() ;
 
+	// go deeper as long as not reaching ground
 	if ( lp_general.depth>depth ) {
+		// retrieve data (SBT record) of thing being hit
 		const Optics optics = *reinterpret_cast<Optics*>( optixGetSbtDataPointer() ) ;
 
+		// retrieve index of triangle (primitive) being hit
+		// index is same as in OptixBuildInput array handed to optixAccelBuild()
 		const int   prix = optixGetPrimitiveIndex() ;
 		const uint3 trix = optics.ices[prix] ;
 
+		// use index to access triangle vertices
 		const float3 A = optics.vces[trix.x] ;
 		const float3 B = optics.vces[trix.y] ;
 		const float3 C = optics.vces[trix.z] ;
 
+		// retrieve triangle barycentric coordinates of hit
 		const float2 bary = optixGetTriangleBarycentrics() ;
 		const float u = bary.x ;
 		const float v = bary.y ;
 		const float w = 1.f-u-v ;
 
-		// calculate primitive hit point
-		const float3 ori = w*A+u*B+v*C ;
+		// calculate world coordinates of hit
+		const float3 hit = w*A+u*B+v*C ;
 
 		// calculate primitive normal
 		const float3 d = optixGetWorldRayDirection() ;
@@ -146,31 +157,36 @@ extern "C" __global__ void __closesthit__diffuse() {
 		if ( V::dot( d, N )>0.f )
 			N = -N ;
 
-		// retrieve and assemble curandState pointer from payload
-		unsigned int sh = optixGetPayload_3() ;
+		// assemble RNG pointer from payload
+		unsigned int sh = optixGetPayload_3() ; // used as well to propagate PNG further down
 		unsigned int sl = optixGetPayload_4() ;
 		curandState* state = reinterpret_cast<curandState*>( static_cast<unsigned long long>( sh )<<32|sl ) ;
 
+		// finally the diffuse reflection according to RTOW
 		const float3 dir = N+V::rndVon1sphere( state ) ;
 
+		// payloads to carry back color
 		unsigned int r, g, b ;
+
+		// one step beyond (recursion)
 		optixTrace(
 				lp_general.as_handle,
-				ori,
-				dir,
-				1e-3f,                      // Min intersection distance
-				1e16f,                      // Max intersection distance
-				0.f,                        // rayTime -- used for motion blur
-				OptixVisibilityMask( 255 ), // Specify always visible
+				hit,                         // ray position
+				dir,                         // ray direction
+				1e-3f,                       // tmin
+				1e16f,                       // tmax
+				0.f,                         // motion
+				OptixVisibilityMask( 255 ),
 				OPTIX_RAY_FLAG_NONE,
-				0,                          // SBT offset   -- See SBT discussion
-				1,                          // SBT stride   -- See SBT discussion
-				0,                          // missSBTIndex -- See SBT discussion
-				r, g, b,
-				sh, sl,
-				++depth
+				0,                           // SBT related
+				1,                           // SBT related
+				0,                           // SBT related
+				r, g, b, // payload upstream propagation: color
+				sh, sl,  // payload downstream propagation: RNG state pointer
+				++depth  // payload downstream propagation: recursion depth
 				) ;
 
+		// update this ray's diffuse color according to RTOW and propagate
 		const float3 albedo = optics.diffuse.albedo ;
 		optixSetPayload_0( __float_as_uint( albedo.x*__uint_as_float( r ) ) ) ;
 		optixSetPayload_1( __float_as_uint( albedo.y*__uint_as_float( g ) ) ) ;
