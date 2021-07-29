3a2aaa9ddaad40c5bfacb9ad268a46ea546e0557
otabuzzman <iuergen.schuck@gmail.com>
2021-07-29 07:21:10 +0200

    Improve API error reporting

--- optx/util_cpu.h
+++ optx/util_cpu.h
@@ -7,47 +7,51 @@
 #include <sstream>
 
 #define CUDA_CHECK( api ) \
-	do {                                                   \
-		cudaError_t e = api ;                              \
-		if ( e != cudaSuccess ) {                          \
-			std::ostringstream comment ;                   \
-			comment                                        \
-				<< "CUDA error: " << #api << " : "         \
-				<< cudaGetErrorString( e ) << "\n" ;       \
-			throw std::runtime_error( comment.str() ) ;    \
-		}                                                  \
+	do {                                                    \
+		cudaError_t e = api ;                               \
+		if ( e != cudaSuccess ) {                           \
+			std::ostringstream comment ;                    \
+			comment                                         \
+				<< "CUDA error: " << #api << " : "          \
+				<< cudaGetErrorString( e ) << std::endl ;   \
+			throw std::runtime_error( comment.str() ) ;     \
+		}                                                   \
 	} while ( false )
 
-#define OPTX_CHECK( api )                                  \
-	do {                                                   \
-		if ( api != OPTIX_SUCCESS ) {                      \
-			std::ostringstream comment ;                   \
-			comment << "OPTX error: " << #api << "\n" ;    \
-			throw std::runtime_error( comment.str() ) ;    \
-		}                                                  \
+#define OPTX_CHECK( api )                                   \
+	do {                                                    \
+		OptixResult e = api ;                               \
+		if ( e != OPTIX_SUCCESS ) {                         \
+			std::ostringstream comment ;                    \
+			comment << "OPTX error: " << #api << " : "      \
+			<< optixGetErrorString( e ) << std::endl ;      \
+			throw std::runtime_error( comment.str() ) ;     \
+		}                                                   \
 	} while ( false )
 
-#define OPTX_CHECK_LOG( api )                              \
-	do {                                                   \
-		char   log[512] ;                                  \
-		size_t sizeof_log = sizeof( log ) ;                \
-		if ( api != OPTIX_SUCCESS ) {                      \
-			std::ostringstream comment ;                   \
-			comment << "OPTX error: " << #api << " : "     \
-			<< log << "\n" ;                               \
-			throw std::runtime_error( comment.str() ) ;    \
-		}                                                  \
+#define OPTX_CHECK_LOG( api )                               \
+	do {                                                    \
+		char   log[512] ;                                   \
+		size_t sizeof_log = sizeof( log ) ;                 \
+		OptixResult e = api ;                               \
+		if ( e != OPTIX_SUCCESS ) {                         \
+			std::ostringstream comment ;                    \
+			comment << "OPTX error: " << #api << " : "      \
+			<< optixGetErrorString( e ) << " : "            \
+			<< log << std::endl ;                           \
+			throw std::runtime_error( comment.str() ) ;     \
+		}                                                   \
 	} while ( false )
 
-#define GLFW_CHECK( api )                                  \
-	do {                                                   \
-		api ;                                              \
-		const char* m ;                                    \
-		if ( glfwGetError( &m ) != GLFW_NO_ERROR ) {       \
-			std::ostringstream comment ;                   \
-			comment << "GLFW error: " << m << std::endl ;  \
-			throw std::runtime_error( comment.str() ) ;    \
-		}                                                  \
+#define GLFW_CHECK( api )                                   \
+	do {                                                    \
+		api ;                                               \
+		const char* m ;                                     \
+		if ( glfwGetError( &m ) != GLFW_NO_ERROR ) {        \
+			std::ostringstream comment ;                    \
+			comment << "GLFW error: " << m << std::endl ;   \
+			throw std::runtime_error( comment.str() ) ;     \
+		}                                                   \
 	} while ( false )
 
 #define GL_CHECK( api )                                     \
@@ -56,18 +60,18 @@
 		GLenum e ;                                          \
 		if ( ( e = glGetError() ) != GL_NO_ERROR ) {        \
 			std::ostringstream comment ;                    \
-			comment << "GL error: " << #api << " :" ;       \
-			do {                                                                                                    \
-				switch ( e ) {                                                                                      \
-					case GL_INVALID_ENUM:                  comment << " GL_INVALID_ENUM" ; break ;                  \
-					case GL_INVALID_VALUE:                 comment << " GL_INVALID_VALUE" ; break ;                 \
-					case GL_INVALID_OPERATION:             comment << " GL_INVALID_OPERATION" ; break ;             \
-					case GL_INVALID_FRAMEBUFFER_OPERATION: comment << " GL_INVALID_FRAMEBUFFER_OPERATION" ; break ; \
-					case GL_OUT_OF_MEMORY:                 comment << " GL_OUT_OF_MEMORY" ; break ;                 \
-					default:                               comment << " " << e << " (undefined)" ; break ;          \
-				}                                                                                                   \
-			} while ( ( e = glGetError() ) != GL_NO_ERROR ) ;                                                       \
-			comment << std::endl ;                                                                                  \
+			comment << "GL error: " << #api << " : " ;      \
+			do {                                                                                                   \
+				switch ( e ) {                                                                                     \
+					case GL_INVALID_ENUM:                  comment << "GL_INVALID_ENUM" ; break ;                  \
+					case GL_INVALID_VALUE:                 comment << "GL_INVALID_VALUE" ; break ;                 \
+					case GL_INVALID_OPERATION:             comment << "GL_INVALID_OPERATION" ; break ;             \
+					case GL_INVALID_FRAMEBUFFER_OPERATION: comment << "GL_INVALID_FRAMEBUFFER_OPERATION" ; break ; \
+					case GL_OUT_OF_MEMORY:                 comment << "GL_OUT_OF_MEMORY" ; break ;                 \
+					default:                               comment << e << " (undefined)" ; break ;                \
+				}                                                                                                  \
+			} while ( ( e = glGetError() ) != GL_NO_ERROR ) ;                                                      \
+			comment << std::endl ;                                                                                 \
 			throw std::runtime_error( comment.str() ) ;     \
 		}                                                   \
 	} while ( false )
