e6e517051f4bcfaf5d2790bb270a673aed32e60d
otabuzzman <iuergen.schuck@gmail.com>
2021-11-29 13:29:08 +0100

    Save work on scene edit feature

--- optx/simplesm.cxx
+++ optx/simplesm.cxx
@@ -662,9 +662,10 @@ void SimpleSM::eaOdiMov() {
 		SmParam* smparam = static_cast<SmParam*>( glfwGetWindowUserPointer( window_ ) ) ;
 		float transform[12] ;
 		scene->get( smparam->pick_id, &transform[0] ) ;
-		// retrieve thing's SR matrix from instance transform
+		// retrieve data from instance transform
+		const float3 pat = { transform[0*4+3], transform[1*4+3], transform[2*4+3] } ; // thing's center position
 		float msr[9] = {
-			transform[0*4+0], transform[0*4+1], transform[0*4+2],
+			transform[0*4+0], transform[0*4+1], transform[0*4+2],                     // thing's SR matrix
 			transform[1*4+0], transform[1*4+1], transform[1*4+2],
 			transform[2*4+0], transform[2*4+1], transform[2*4+2],
 		} ;
@@ -672,24 +673,32 @@ void SimpleSM::eaOdiMov() {
 		double x, y ;
 		float lo, la ;
 		glfwGetCursorPos( window_, &x, &y ) ;
-		paddle_->move( static_cast<int>( x ), static_cast<int>( y ), &lo, &la ) ; // x/ y rotations in radiants (lo/ la)
-		const float cosx = cosf( lo ) ;
+		paddle_->move( static_cast<int>( x ), static_cast<int>( y ), &lo, &la ) ;     // rotation angles (lo/ la) in radiants
+		const float cosx = cosf( la ) ;
 		const float sinx = sqrtf( 1.f-cosx*cosx ) ;
+		Camera& camera = lp_general.camera ;
+		const float3 vex = V::unitV( V::cross( pat-camera.eye(), camera.vup() ) ) ;   // x-axis in terms of view direction
+//		const float3 vex = { 1.f, 0.f, 0.f } ;
 		float rox[9] = {
-			1.f,  0.f,   0.f,
-			0.f, cosx, -sinx,
-			0.f, sinx,  cosx
+			vex.x*vex.x*( 1-cosx )+      cosx, vex.x*vex.y*( 1-cosx )-vex.z*sinx, vex.x*vex.z*( 1-cosx )+vex.y*sinx,
+			vex.y*vex.x*( 1-cosx )+vex.z*sinx, vex.y*vex.y*( 1-cosx )+      cosx, vex.y*vex.z*( 1-cosx )-vex.x*sinx,
+			vex.z*vex.x*( 1-cosx )-vex.y*sinx, vex.z*vex.y*( 1-cosx )+vex.x*sinx, vex.z*vex.z*( 1-cosx )+      cosx
 		} ;
-		if ( 0.f>lo )
+//		float rox[9] = {
+//			1.f,  0.f,   0.f,
+//			0.f, cosx, -sinx,
+//			0.f, sinx,  cosx
+//		} ;
+		if ( 0.f>la )
 			mattrs<3>( &rox[0] ) ;
-		const float cosy = cosf( la ) ;
+		const float cosy = cosf( lo ) ;
 		const float siny = sqrtf( 1.f-cosy*cosy ) ;
 		float roy[9] = {
 			 cosy, 0.f, siny,
 			  0.f, 1.f, 0.f,
 			-siny, 0.f, cosy
 		} ;
-		if ( 0.f>la )
+		if ( 0.f>lo )
 			mattrs<3>( &roy[0] ) ;
 		// combine SR with x/ y rotate matrices
 		matmul<3>( &msr[0], &rox[0] ) ;
